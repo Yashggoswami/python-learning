c6cc3781ccd4c21a85054cf5aa71c03cce309240
Yashggoswami <yashgoswami2000@gmail.com>
2021-09-05 19:29:53 +0530

    variables learning

--- piramid.py
+++ piramid.py
@@ -3,7 +3,7 @@ num1 = int(input())
 def piramid(num1=0):
     if num1 == 0:
         return
-    for i in range(num1,0,-1):
+    for i in reversed(range(1,num1+1)):
        print("*"*(i))
     
 piramid(num1=num1)
---
+++ variable.py
@@ -0,0 +1,36 @@
+# num1 = float(input())
+# num1 = "first"
+# print(type(num1))
+
+# camel case
+myVariableName = 122434
+# pascal case
+MyVariableName = "yash goswami"
+# snake case
+my_variable_name = 2134.534
+
+
+# red,orange,green = 1,23,34
+# print(red,orange,green)
+
+first = second = three = 12
+
+# print(first,second,three)
+
+# If you have a collection of values in a list, tuple etc. Python allows you extract the values into variables. This is called unpacking.
+
+
+list = ("first","second","three","four","five","six","seven")
+
+first,second,*list = list
+
+# print(first,second,three)
+
+# print(first)
+# print(second)
+# print(list)
+
+num  = 122
+string = "yash"
+print(string+str(num))
+
