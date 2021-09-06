46447b7ad957743d107a91a4edc1b9c2d25cda57
Yashggoswami <yashgoswami2000@gmail.com>
2021-09-06 18:10:55 +0530

    learning string and made pramid pattern

--- stringlearning.py
+++ stringlearning.py
@@ -9,7 +9,20 @@
 # print(st1.center(121))
 
 # pattern to make pramid using string.center() function
-if __name__ == "__main__":
-  num  = int(input())
-  for i in range(1,num+1):
-      print(("*"*i).center(num+1))
+# if __name__ == "__main__":
+#   num  = int(input())
+#   for i in range(1,num+1):
+#       print(("*"*i).center(num+1))
+
+
+# st = "HELLO world"
+# print(st.swapcase())
+
+# st = "124"
+# print(st.isdigit())
+
+list1 = ['yash','vinay','goswami','ananaya','dodo','basss']
+
+print(list1[-1:0:-2])
+# list1.sort()
+# list1.reverse()
